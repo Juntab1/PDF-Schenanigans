4a35f2f52e98202301065f2213b0545edd6db601
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2024-03-12 17:59:41 -0700

    now able to encrypt and decrypt

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 from pypdf import PdfReader, PdfWriter
 
+# Write the provided pdf to be able to 
+# do functions such as encrypt
 def write_pages(write_pdf, read):
     for page in read.pages:
         write_pdf.add_page(page)
 
+# Creates a new pdf to be returned
 def new_pdf(write_pdf):
-    with open("new-dummy.pdf", "wb") as output:
+    with open("decrypted-dummy.pdf", "wb") as output:
         write_pdf.write(output)
 
+# This function encrypts a pdf with a password
+def encrypt_pdf(write_pdf):
+    # AES-256: gov adopted symmetric-key encryption algorithm
+    write_pdf.encrypt("password", algorithm="AES-256")
+
+# This function decrypts a encrypted pdf provided by the user
+def decrypt_given_pdf(read, password):
+    if read.is_encrypted:
+        read.decrypt(password)
 
 def main():
     read = PdfReader("dummy.pdf")
@@ -18,7 +30,5 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# look at the documentation and maybe work on encrypting next that would be fun
 
 
