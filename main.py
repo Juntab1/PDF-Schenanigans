96aae3baf0e83b9f3f887b647ff9f62e063a7d98
Juntaro Bruttomesso <jsbwhaleguy@gmail.com>
2024-03-12 17:37:14 -0700

    OOP

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
 from pypdf import PdfReader, PdfWriter
 
-read = PdfReader("dummy.pdf")
-write_pdf = PdfWriter()
+def write_pages(write_pdf, read):
+    for page in read.pages:
+        write_pdf.add_page(page)
 
-# this writes the one page in read
-for page in read.pages:
-    write_pdf.add_page(page)
+def new_pdf(write_pdf):
+    with open("new-dummy.pdf", "wb") as output:
+        write_pdf.write(output)
 
-# save for new PDF to a file
-with open("new-dummy.pdf", "wb") as output:
-    write_pdf.write(output)
 
+def main():
+    read = PdfReader("dummy.pdf")
+    write_pdf = PdfWriter()
+
+    write_pages(write_pdf, read)
+    new_pdf(write_pdf)
+
+if __name__ == "__main__":
+    main()
 
 # look at the documentation and maybe work on encrypting next that would be fun
 
